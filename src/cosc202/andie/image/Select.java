37936774052fee1dad240fa3d21f4695445a302f
Nic Scott <nicholaswilliamscott1@gmail.com>
2023-05-15 11:49:57 +1200

    Finally implemented select     Selected area is highlighted in a transperant blue     Click, drag and release to select area     Click & release to unselect

--- src/cosc202/andie/image/Select.java
+++ src/cosc202/andie/image/Select.java
@@ -2,13 +2,18 @@ package cosc202.andie.image;
 
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Point;
 
 import javax.swing.event.MouseInputAdapter;
 
 import cosc202.andie.ImagePanel;
 
-public class Select extends ImagePanel implements ImageOperation{
+public class Select implements ImageOperation{
     private BufferedImage input;
     private BufferedImage original;
     private ImagePanel target;
@@ -23,18 +28,68 @@ public class Select extends ImagePanel implements ImageOperation{
 
     public void setTarget(ImagePanel target) {
         this.target = target;
+        SelectMouseMotionListener selectListener = new SelectMouseMotionListener(this);
+        target.addMouseListener(selectListener);
+        target.addMouseMotionListener(selectListener);
     }
 
     public BufferedImage apply(BufferedImage input) {
         this.input = input;
-        this.original = input;
-        SelectMouseMotionListener selectListener = new SelectMouseMotionListener();
-        target.addMouseListener(selectListener);
-        target.addMouseMotionListener(selectListener);
+        this.original = deepCopy(input);
+        // Create a graphics context from the input image
+        Graphics2D g2d = input.createGraphics();
+
+        // Set the drawing color to blue with an alpha value of 0.5
+        Color blueSemiTransparent = new Color(0f, 0f, 1f, 0.25f);
+        g2d.setColor(blueSemiTransparent);
+
+        // Draw a rectangle using the start and end points
+        int x = Math.min(start.x, end.x);
+        int y = Math.min(start.y, end.y);
+        int width = Math.abs(start.x - end.x);
+        int height = Math.abs(start.y - end.y);
+        g2d.fillRect(x, y, width, height);
+
+        // Set the composite to opaque to restore normal rendering
+        g2d.setComposite(AlphaComposite.SrcOver);
+
+        // Dispose of the graphics context to free up resources
+        g2d.dispose();
+
+        isSelectionApplied = true;
         return input;
     }
 
+    public void revert() {
+        if (isSelectionApplied) {
+            // Copy the original image to the input image to restore it
+            Graphics2D g2d = input.createGraphics();
+            g2d.drawImage(original, 0, 0, null);
+            g2d.dispose();
+            
+            // Reset selection variables
+            start = null;
+            end = null;
+            isSelectionApplied = false;
+    
+            // Repaint the target panel
+            target.repaint();
+        }
+    }
+
+    private static BufferedImage deepCopy(BufferedImage bi) {
+        ColorModel cm = bi.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = bi.copyData(null);
+        return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+    }
+
     private class SelectMouseMotionListener extends MouseInputAdapter {
+        private Select select;
+
+        public SelectMouseMotionListener(Select select) {
+            this.select = select;
+        }
         /**
          * Identifies the initial coordinates when the mouse is pressed/dragged
          */
@@ -46,10 +101,14 @@ public class Select extends ImagePanel implements ImageOperation{
          * Identifies the final co-ordinates where the mouse is released
          */
         public void mouseReleased(MouseEvent e) {
-            setEnd(e.getPoint());
-            target.getImage().apply(new SelectHighlighter(start, end));
-            target.repaint();
-            target.getParent().revalidate();
+            if(isSelectionApplied){
+                revert();
+            }else{
+                setEnd(e.getPoint());
+                target.getImage().apply(select);
+                target.repaint();
+                target.getParent().revalidate();
+            }
         }
 
         /**
@@ -67,9 +126,6 @@ public class Select extends ImagePanel implements ImageOperation{
             end = p;
         }
 
-    }
-
-    public void revert() {
     }
     
 }
